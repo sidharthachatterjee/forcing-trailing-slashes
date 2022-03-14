cd29627187b776929a2f4218574775b877605f79
Sid Chatterjee <sid@cloudflare.com>
2022-03-14 13:21:44 +0000

    Add _worker

---
+++ public/_worker.js
@@ -0,0 +1,48 @@
+export default {
+  async fetch(request, env) {
+    let { pathname, origin, search } = new URL(request.url);
+
+    if (pathname !== "/") {
+      // Serve requests with trailing slashes
+      if (pathname.endsWith("/")) {
+        const assetUrlWithoutTrailingSlash = new URL(
+          pathname.slice(0, -1),
+          origin
+        );
+
+        const assetEntry = await env.ASSETS.fetch(
+          assetUrlWithoutTrailingSlash,
+          request
+        );
+
+        if (assetEntry.status === 200) {
+          return assetEntry;
+        }
+      }
+      // Redirect requests without trailing slashes
+      else {
+        const assetUrlWithTrailingSlash = new URL(
+          `${pathname}/${search}`,
+          origin
+        );
+
+        const assetEntry = await env.ASSETS.fetch(
+          assetUrlWithTrailingSlash,
+          request
+        );
+
+        if (assetEntry.status !== 404) {
+          return new Response(null, {
+            status: 301,
+            headers: {
+              Location: assetUrlWithTrailingSlash,
+            },
+          });
+        }
+      }
+    }
+
+    // Fall back to asset server
+    return env.ASSETS.fetch(request.url, request);
+  },
+};
